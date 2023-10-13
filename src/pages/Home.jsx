e89f7ecaa783d107b72dc6144abf9a04915b73ea
Souvik Roy <125180450+git-souvik22@users.noreply.github.com>
2023-10-13 09:40:59 +0530

    it-work-1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -49,8 +49,8 @@ const Home = () => {
                 id="menu"
               >
                 <li className="nav-item">
-                  <a href="#" className="nav-link align-middle px-0">
-                    <i className="fs-4 bi-house" />{" "}
+                  <a href="#nav" className="nav-link align-middle px-0">
+                    <i className="fs-4 bi-house" />
                     <span className="ms-1 d-none d-sm-inline">Home</span>
                   </a>
                 </li>
@@ -60,8 +60,8 @@ const Home = () => {
                     data-bs-toggle="collapse"
                     className="nav-link px-0 align-middle"
                   >
-                    <i className="fs-4 bi-speedometer2" />{" "}
-                    <span className="ms-1 d-none d-sm-inline">Dashboard</span>{" "}
+                    <i className="fs-4 bi-speedometer2" />
+                    <span className="ms-1 d-none d-sm-inline">Dashboard</span>
                   </a>
                   <ul
                     className="collapse show nav flex-column ms-1"
@@ -69,22 +69,20 @@ const Home = () => {
                     data-bs-parent="#menu"
                   >
                     <li className="w-100">
-                      <a href="#" className="nav-link px-0">
-                        {" "}
-                        <span className="d-none d-sm-inline">Item</span> 1{" "}
+                      <a href="#nav" className="nav-link px-0">
+                        <span className="d-none d-sm-inline">Item</span> 1
                       </a>
                     </li>
                     <li>
-                      <a href="#" className="nav-link px-0">
-                        {" "}
-                        <span className="d-none d-sm-inline">Item</span> 2{" "}
+                      <a href="#nav" className="nav-link px-0">
+                        <span className="d-none d-sm-inline">Item</span> 2
                       </a>
                     </li>
                   </ul>
                 </li>
                 <li>
-                  <a href="#" className="nav-link px-0 align-middle">
-                    <i className="fs-4 bi-table" />{" "}
+                  <a href="#nav" className="nav-link px-0 align-middle">
+                    <i className="fs-4 bi-table" />
                     <span className="ms-1 d-none d-sm-inline">Orders</span>
                   </a>
                 </li>
@@ -94,7 +92,7 @@ const Home = () => {
                     data-bs-toggle="collapse"
                     className="nav-link px-0 align-middle "
                   >
-                    <i className="fs-4 bi-bootstrap" />{" "}
+                    <i className="fs-4 bi-bootstrap" />
                     <span className="ms-1 d-none d-sm-inline">Bootstrap</span>
                   </a>
                   <ul
@@ -103,14 +101,12 @@ const Home = () => {
                     data-bs-parent="#menu"
                   >
                     <li className="w-100">
-                      <a href="#" className="nav-link px-0">
-                        {" "}
+                      <a href="#nav" className="nav-link px-0">
                         <span className="d-none d-sm-inline">Item</span> 1
                       </a>
                     </li>
                     <li>
-                      <a href="#" className="nav-link px-0">
-                        {" "}
+                      <a href="#nav" className="nav-link px-0">
                         <span className="d-none d-sm-inline">Item</span> 2
                       </a>
                     </li>
@@ -122,8 +118,8 @@ const Home = () => {
                     data-bs-toggle="collapse"
                     className="nav-link px-0 align-middle"
                   >
-                    <i className="fs-4 bi-grid" />{" "}
-                    <span className="ms-1 d-none d-sm-inline">Products</span>{" "}
+                    <i className="fs-4 bi-grid" />
+                    <span className="ms-1 d-none d-sm-inline">Products</span>
                   </a>
                   <ul
                     className="collapse nav flex-column ms-1"
@@ -131,42 +127,38 @@ const Home = () => {
                     data-bs-parent="#menu"
                   >
                     <li className="w-100">
-                      <a href="#" className="nav-link px-0">
-                        {" "}
+                      <a href="#nav" className="nav-link px-0">
                         <span className="d-none d-sm-inline">Product</span> 1
                       </a>
                     </li>
                     <li>
-                      <a href="#" className="nav-link px-0">
-                        {" "}
+                      <a href="#nav" className="nav-link px-0">
                         <span className="d-none d-sm-inline">Product</span> 2
                       </a>
                     </li>
                     <li>
-                      <a href="#" className="nav-link px-0">
-                        {" "}
+                      <a href="#nav" className="nav-link px-0">
                         <span className="d-none d-sm-inline">Product</span> 3
                       </a>
                     </li>
                     <li>
-                      <a href="#" className="nav-link px-0">
-                        {" "}
+                      <a href="#nav" className="nav-link px-0">
                         <span className="d-none d-sm-inline">Product</span> 4
                       </a>
                     </li>
                   </ul>
                 </li>
                 <li>
-                  <a href="#" className="nav-link px-0 align-middle">
-                    <i className="fs-4 bi-people" />{" "}
-                    <span className="ms-1 d-none d-sm-inline">Customers</span>{" "}
+                  <a href="#nav" className="nav-link px-0 align-middle">
+                    <i className="fs-4 bi-people" />
+                    <span className="ms-1 d-none d-sm-inline">Customers</span>
                   </a>
                 </li>
               </ul>
               <hr />
               <div className="dropdown pb-4">
                 <a
-                  href="#"
+                  href="#nav"
                   className="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
                   id="dropdownUser1"
                   data-bs-toggle="dropdown"
@@ -183,17 +175,17 @@ const Home = () => {
                 </a>
                 <ul className="dropdown-menu dropdown-menu-dark text-small shadow">
                   <li>
-                    <a className="dropdown-item" href="#">
+                    <a className="dropdown-item" href="#nav">
                       New project...
                     </a>
                   </li>
                   <li>
-                    <a className="dropdown-item" href="#">
+                    <a className="dropdown-item" href="#nav">
                       Settings
                     </a>
                   </li>
                   <li>
-                    <a className="dropdown-item" href="#">
+                    <a className="dropdown-item" href="#nav">
                       Profile
                     </a>
                   </li>
@@ -201,7 +193,7 @@ const Home = () => {
                     <hr className="dropdown-divider" />
                   </li>
                   <li>
-                    <a className="dropdown-item" href="#">
+                    <a className="dropdown-item" href="#nav">
                       Sign out
                     </a>
                   </li>
